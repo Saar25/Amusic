34f395fec18ffea833cd89282152d308cce91ec2
Saar Tako <saartako25@gmail.com>
2025-02-02 17:22:08 +0200

    showing song data in card

--- client/src/main/java/org/saartako/client/controllers/SongController.java
+++ client/src/main/java/org/saartako/client/controllers/SongController.java
@@ -3,14 +3,50 @@ package org.saartako.client.controllers;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import org.saartako.song.Song;
 
 public class SongController {
 
-    private ObjectProperty<Song> songProperty = new SimpleObjectProperty<>(null, "song", null);
+    private final ObjectProperty<Song> songProperty = new SimpleObjectProperty<>(null, "song", null);
+
+    @FXML
+    private Label songNameLabel;
+
+    @FXML
+    private VBox footerVBox;
 
     @FXML
     private void initialize() {
+        this.songProperty.addListener((o, prev, song) -> {
+            if (song != null) {
+                this.songNameLabel.setText(song.getName());
+            }
+
+            this.footerVBox.getChildren().clear();
+
+            if (song != null && song.getUploader() != null) {
+                this.footerVBox.getChildren().add(new Label(
+                    "By: " + song.getUploader().getDisplayName()
+                ));
+            }
+            if (song != null) {
+                this.footerVBox.getChildren().add(new Label(
+                    "Length: " + "TODO"
+                ));
+            }
+            if (song != null && song.getGenre() != null) {
+                this.footerVBox.getChildren().add(new Label(
+                    "Genre: " + song.getGenre().getName()
+                ));
+            }
+            if (song != null && song.getLanguage() != null) {
+                this.footerVBox.getChildren().add(new Label(
+                    "Language: " + song.getLanguage().getName()
+                ));
+            }
+        });
     }
 
     public void setSong(Song song) {
