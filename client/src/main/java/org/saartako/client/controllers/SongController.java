74fc1a4d9e93a938fd82748b3598a651708a2df8
Saar Tako <saartako25@gmail.com>
2025-02-02 17:02:19 +0200

    showing fetched songs on grid

--- client/src/main/java/org/saartako/client/controllers/SongController.java
+++ client/src/main/java/org/saartako/client/controllers/SongController.java
@@ -1,4 +1,19 @@
 package org.saartako.client.controllers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import org.saartako.song.Song;
+
 public class SongController {
+
+    private ObjectProperty<Song> songProperty = new SimpleObjectProperty<>(null, "song", null);
+
+    @FXML
+    private void initialize() {
+    }
+
+    public void setSong(Song song) {
+        this.songProperty.setValue(song);
+    }
 }
--- client/src/main/java/org/saartako/client/controllers/SongsPageController.java
+++ client/src/main/java/org/saartako/client/controllers/SongsPageController.java
@@ -1,12 +1,18 @@
 package org.saartako.client.controllers;
 
 import atlantafx.base.controls.CustomTextField;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.GridPane;
 import org.saartako.client.services.SongService;
+import org.saartako.song.Song;
 
-import java.util.Arrays;
+import java.io.IOException;
 
 public class SongsPageController {
 
@@ -15,7 +21,10 @@ public class SongsPageController {
     private final StringProperty searchProperty = new SimpleStringProperty(this, "searchProperty", "");
 
     @FXML
-    public CustomTextField searchTextField;
+    private CustomTextField searchTextField;
+
+    @FXML
+    private GridPane songsGridPane;
 
     @FXML
     private void initialize() {
@@ -23,11 +32,34 @@ public class SongsPageController {
 
         this.songService.fetchSongs().whenComplete((songs, error) -> {
             if (songs != null) {
-                System.out.println("songs: " + Arrays.toString(songs));
+                Platform.runLater(() -> addSongsToGrid(songs));
             }
             if (error != null) {
-                System.err.println("error: " + error.getMessage());
+                Platform.runLater(() -> {
+                    final Alert alert = new Alert(
+                        Alert.AlertType.INFORMATION,
+                        "Failed to fetch songs\n" + error.getMessage());
+                    alert.show();
+                });
             }
         });
+    }
+
+    private void addSongsToGrid(Song[] songs) {
+        try {
+            this.songsGridPane.getChildren().clear();
+
+            for (int i = 0; i < songs.length; i++) {
+                final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/components/song.fxml"));
+                final Node songNode = fxmlLoader.load();
+
+                final SongController controller = fxmlLoader.getController();
+                controller.setSong(songs[i]);
+
+                this.songsGridPane.add(songNode, i % 3, i / 3);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
