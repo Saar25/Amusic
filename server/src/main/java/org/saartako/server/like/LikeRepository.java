78af0e447b6a67b579f06c13205abe9ab2456a00
Saar Tako <saartako25@gmail.com>
2025-03-02 15:40:55 +0200

    implemented useful methods in LikeRepository

--- server/src/main/java/org/saartako/server/like/LikeRepository.java
+++ server/src/main/java/org/saartako/server/like/LikeRepository.java
@@ -1,6 +1,17 @@
 package org.saartako.server.like;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
+
+    @Query("SELECT COUNT(sl) FROM likes sl WHERE sl.song.id = :songId")
+    long findSongLikeCount(@Param("songId") Long songId);
+
+    @Query("SELECT sl.song.id FROM likes sl WHERE sl.user.id = :userId")
+    List<Long> findLikedSongIdsByUserId(@Param("userId") Long userId);
+
 }
--- server/src/main/java/org/saartako/server/user/UserRepository.java
+++ server/src/main/java/org/saartako/server/user/UserRepository.java
@@ -11,7 +11,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Query("SELECT p FROM users p WHERE p.username = :username")
     Optional<UserEntity> findByUsername(@Param("username") String username);
 
-    @Query("SELECT p FROM users p LEFT JOIN FETCH p.roles WHERE p.username = :username ")
+    @Query("SELECT p FROM users p LEFT JOIN FETCH p.roles WHERE p.username = :username")
     Optional<UserEntity> findByUsernameWithRoles(@Param("username") String username);
 
 }
