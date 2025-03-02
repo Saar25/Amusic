c7c3b264749ad8c60aad7259fdd3dc811bb89ff3
Saar Tako <saartako25@gmail.com>
2025-03-02 17:00:10 +0200

    fixed unlike songs

--- server/src/main/java/org/saartako/server/like/LikeRepository.java
+++ server/src/main/java/org/saartako/server/like/LikeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
     List<Long> findLikedSongIdsByUserId(@Param("userId") Long userId);
 
     @Modifying
+    @Transactional
     @Query("DELETE FROM likes sl WHERE sl.user.id = :userId AND sl.song.id = :songId")
     void unlikeSong(@Param("userId") Long userId, @Param("songId") Long songId);
 
