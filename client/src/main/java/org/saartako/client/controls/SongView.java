2e63b6836ad13faf4f0b23955e078cd6ac0b8390
Saar Tako <saartako25@gmail.com>
2025-03-08 20:34:39 +0200

    moved all logic from skin to control in song view

--- client/src/main/java/org/saartako/client/controls/SongView.java
+++ client/src/main/java/org/saartako/client/controls/SongView.java
@@ -1,20 +1,41 @@
 package org.saartako.client.controls;
 
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.ListBinding;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Control;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.util.Duration;
+import javafx.util.StringConverter;
+import org.saartako.client.Config;
 import org.saartako.client.enums.SongPlayerStatus;
 import org.saartako.client.models.RouteNode;
+import org.saartako.client.services.AuthService;
 import org.saartako.client.services.PlaylistService;
 import org.saartako.client.services.RouterService;
 import org.saartako.client.services.SongService;
+import org.saartako.client.utils.BindingsUtils;
 import org.saartako.common.playlist.Playlist;
 import org.saartako.common.song.Song;
+import org.saartako.common.user.User;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 public class SongView extends Control implements RouteNode {
+
+    private final SongService songService = SongService.getInstance();
+    private final PlaylistService playlistService = PlaylistService.getInstance();
+    private final AuthService authService = AuthService.getInstance();
+    private final RouterService routerService = RouterService.getInstance();
 
     private final ObjectProperty<Duration> currentSongTime =
         new SimpleObjectProperty<>(this, "currentSongTime", Duration.ZERO);
@@ -22,51 +43,32 @@ public class SongView extends Control implements RouteNode {
     private final ObjectProperty<SongPlayerStatus> songPlayerStatus =
         new SimpleObjectProperty<>(this, "songPlayerStatus", SongPlayerStatus.STOPPED);
 
-    public ObjectProperty<Duration> currentSongTimeProperty() {
-        return this.currentSongTime;
-    }
+    private final BooleanBinding isSongLiked = Bindings.createBooleanBinding(() -> {
+        final Song song = this.songService.currentSongProperty().get();
+        if (song == null) return false;
+        final ListProperty<Long> likedSongIds = this.songService.likedSongIdsProperty();
+        if (likedSongIds == null) return false;
+        return likedSongIds.contains(song.getId());
+    }, this.songService.currentSongProperty(), this.songService.likedSongIdsProperty());
 
-    public ObjectProperty<SongPlayerStatus> songPlayerStatusProperty() {
-        return this.songPlayerStatus;
-    }
+    private final BooleanBinding isSongPersonal = Bindings.createBooleanBinding(() -> {
+        final Song song = this.songService.currentSongProperty().get();
+        if (song == null) return false;
+        final User user = this.authService.loggedUserProperty().get();
+        if (user == null) return false;
+        return song.getUploader().getId() == user.getId();
+    }, this.songService.currentSongProperty(), this.authService.loggedUserProperty());
 
-    public void addCurrentSongToPlaylist(Playlist playlist) {
-        // TODO: put services as instance field, cannot right now because it ruins lazy fetching
-        final PlaylistService playlistService = PlaylistService.getInstance();
-        final SongService songService = SongService.getInstance();
+    private final ListBinding<Playlist> personalPlaylists = BindingsUtils.createJavaListBinding(() -> {
+        final ListBinding<Playlist> playlists = this.playlistService.playlistsProperty();
+        if (playlists == null) return null;
+        final User user = this.authService.loggedUserProperty().get();
+        if (user == null) return null;
+        return playlists.stream().filter(playlist -> playlist.getOwner().getId() == user.getId()).toList();
+    }, this.songService.currentSongProperty(), this.authService.loggedUserProperty());
 
-        final Song song = songService.getCurrentSong();
-
-        playlistService.addPlaylistSong(playlist, song).whenComplete((response, error) -> {
-            Platform.runLater(() -> {
-                final Alert alert = error != null
-                    ? new Alert(Alert.AlertType.ERROR, "Failed too add song\n" + error.getMessage())
-                    : new Alert(Alert.AlertType.INFORMATION, "Added song to playlist successfully");
-                alert.show();
-            });
-        });
-    }
-
-    public void deleteCurrentSong() {
-        // TODO: put services as instance field, cannot right now because it ruins lazy fetching
-        final SongService songService = SongService.getInstance();
-        final RouterService routerService = RouterService.getInstance();
-
-        final Song song = songService.getCurrentSong();
-
-        songService.deleteSong(song).whenComplete((response, error) -> {
-            Platform.runLater(() -> {
-                final Alert alert;
-                if (error != null) {
-                    alert = new Alert(Alert.AlertType.ERROR, "Failed too delete song\n" + error.getMessage());
-                } else {
-                    alert = new Alert(Alert.AlertType.INFORMATION, "Succeeded to delete song");
-                    alert.resultProperty().addListener(o -> routerService.previous());
-                }
-                alert.show();
-            });
-        });
-    }
+    private final ObjectProperty<String> songAudioStreamUrl =
+        new SimpleObjectProperty<>(this, "songAudioStreamUrl", null);
 
     @Override
     protected SongViewSkin createDefaultSkin() {
@@ -82,5 +84,124 @@ public class SongView extends Control implements RouteNode {
     public void onEnterView() {
         currentSongTimeProperty().set(Duration.ZERO);
         songPlayerStatusProperty().set(SongPlayerStatus.PLAYING);
+        this.songAudioStreamUrl.set(null);
+    }
+
+    public ObjectBinding<Song> currentSongProperty() {
+        return this.songService.currentSongProperty();
+    }
+
+    public BooleanBinding isSongLikedProperty() {
+        return this.isSongLiked;
+    }
+
+    public BooleanBinding isSongPersonalProperty() {
+        return this.isSongPersonal;
+    }
+
+    public ObjectProperty<Duration> currentSongTimeProperty() {
+        return this.currentSongTime;
+    }
+
+    public ObjectProperty<SongPlayerStatus> songPlayerStatusProperty() {
+        return this.songPlayerStatus;
+    }
+
+    public void onLikeSongButtonClick() {
+        final Song song = this.songService.getCurrentSong();
+
+        this.songService.toggleLikeSong(song);
+    }
+
+    public CompletableFuture<String> fetchSongAudioStreamUrl(Song song) {
+        return this.songService.fetchSongAudioStreamUrl(song);
+    }
+
+    public void onAddToPlaylistButtonClick() {
+        final Optional<Playlist> result = openAddToPlaylistDialog();
+
+        result.ifPresent(this::addCurrentSongToPlaylist);
+    }
+
+    public void onDeleteSongButtonClick() {
+        deleteCurrentSong();
+    }
+
+    private Optional<Playlist> openAddToPlaylistDialog() {
+        final Dialog<Playlist> dialog = new Dialog<>();
+        dialog.setTitle("Add to playlist");
+
+        final HBox hBox = new HBox(Config.GAP_LARGE);
+        hBox.setPadding(new Insets(Config.GAP_LARGE));
+        hBox.setAlignment(Pos.CENTER);
+
+        final Label playlistLabel = new Label("Choose playlist:");
+
+        final ComboBox<Playlist> playlistComboBox = createPlaylistComboBox();
+
+        hBox.getChildren().addAll(playlistLabel, playlistComboBox);
+
+        dialog.getDialogPane().setContent(hBox);
+        dialog.getDialogPane().getButtonTypes().addAll(
+            new ButtonType("Add", ButtonBar.ButtonData.OK_DONE),
+            new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE));
+
+        dialog.setResultConverter(button -> {
+            if (button.getButtonData() == ButtonBar.ButtonData.OK_DONE) {
+                return playlistComboBox.getValue();
+            }
+            return null;
+        });
+
+        return dialog.showAndWait();
+    }
+
+    private ComboBox<Playlist> createPlaylistComboBox() {
+        final ComboBox<Playlist> playlistComboBox = new ComboBox<>(this.personalPlaylists);
+        playlistComboBox.setPlaceholder(new Label("Loading..."));
+        playlistComboBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(Playlist object) {
+                return object == null ? "Choose playlist..." : object.getName();
+            }
+
+            @Override
+            public Playlist fromString(String string) {
+                return playlistComboBox.getItems().stream().filter(p ->
+                    Objects.equals(p.getName(), string)).findAny().orElse(null);
+            }
+        });
+
+        return playlistComboBox;
+    }
+
+    private void addCurrentSongToPlaylist(Playlist playlist) {
+        final Song song = this.songService.getCurrentSong();
+
+        this.playlistService.addPlaylistSong(playlist, song).whenComplete((response, error) -> {
+            Platform.runLater(() -> {
+                final Alert alert = error != null
+                    ? new Alert(Alert.AlertType.ERROR, "Failed too add song\n" + error.getMessage())
+                    : new Alert(Alert.AlertType.INFORMATION, "Added song to playlist successfully");
+                alert.show();
+            });
+        });
+    }
+
+    private void deleteCurrentSong() {
+        final Song song = this.songService.getCurrentSong();
+
+        this.songService.deleteSong(song).whenComplete((response, error) -> {
+            Platform.runLater(() -> {
+                final Alert alert;
+                if (error != null) {
+                    alert = new Alert(Alert.AlertType.ERROR, "Failed too delete song\n" + error.getMessage());
+                } else {
+                    alert = new Alert(Alert.AlertType.INFORMATION, "Succeeded to delete song");
+                    alert.resultProperty().addListener(o -> this.routerService.previous());
+                }
+                alert.show();
+            });
+        });
     }
 }
--- client/src/main/java/org/saartako/client/controls/SongViewSkin.java
+++ client/src/main/java/org/saartako/client/controls/SongViewSkin.java
@@ -3,34 +3,23 @@ package org.saartako.client.controls;
 import atlantafx.base.controls.ProgressSliderSkin;
 import atlantafx.base.theme.Styles;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.SkinBase;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
-import javafx.util.StringConverter;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.material2.Material2AL;
 import org.saartako.client.Config;
 import org.saartako.client.enums.SongPlayerStatus;
 import org.saartako.client.models.CardItem;
-import org.saartako.client.services.AuthService;
-import org.saartako.client.services.PlaylistService;
-import org.saartako.client.services.SongService;
 import org.saartako.client.utils.GridUtils;
 import org.saartako.client.utils.SongUtils;
-import org.saartako.common.playlist.Playlist;
 import org.saartako.common.song.Song;
-import org.saartako.common.user.User;
 
-import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public class SongViewSkin extends SkinBase<SongView> {
@@ -40,11 +29,6 @@ public class SongViewSkin extends SkinBase<SongView> {
     private static final FontIcon NOT_FAVORITE_FONT_ICON = new FontIcon(Material2AL.FAVORITE_BORDER);
     private static final FontIcon FAVORITE_FONT_ICON = new FontIcon(Material2AL.FAVORITE);
     private static final FontIcon DELETE_FONT_ICON = new FontIcon(Material2AL.DELETE);
-
-    // TODO: do not put services in skin, get data only from control
-    private final SongService songService = SongService.getInstance();
-    private final PlaylistService playlistService = PlaylistService.getInstance();
-    private final AuthService authService = AuthService.getInstance();
 
     private final Loader loader = new Loader();
 
@@ -88,74 +72,20 @@ public class SongViewSkin extends SkinBase<SongView> {
         this.gridPane.add(vBox, 8, 2, 4, 6);
         this.gridPane.add(this.slider, 0, 11, 12, 1);
 
-        registerChangeListener(
-            getSkinnable().songPlayerStatusProperty(),
-            observable -> updateMediaPlayerStatus());
+        registerChangeListener(getSkinnable().songPlayerStatusProperty(), observable -> updateMediaPlayerStatus());
         updateMediaPlayerStatus();
 
-        registerChangeListener(
-            getSkinnable().currentSongTimeProperty(),
-            observable -> updateSongCurrentTime());
+        registerChangeListener(getSkinnable().currentSongTimeProperty(), observable -> updateSongCurrentTime());
         updateSongCurrentTime();
 
-        registerChangeListener(this.songService.currentSongProperty(), observable -> updateSong());
-        registerChangeListener(this.authService.loggedUserProperty(), observable -> updateSong());
-        registerListChangeListener(this.songService.likedSongIdsProperty(), observable -> updateSong());
-        updateSong();
-    }
+        registerChangeListener(getSkinnable().currentSongProperty(), observable -> updateCurrentSong());
+        updateCurrentSong();
 
-    private void updateSong() {
-        final Song song = this.songService.getCurrentSong();
-        final User user = this.authService.getLoggedUser();
+        registerChangeListener(getSkinnable().isSongLikedProperty(), observable -> updateIsSongLiked());
+        updateIsSongLiked();
 
-        if (song == null || user == null) {
-            Platform.runLater(() -> {
-                getChildren().setAll(this.loader);
-            });
-        } else {
-            createMediaPlayer(song).thenAccept(mediaPlayer -> {
-                if (this.mediaPlayer != null) {
-                    this.mediaPlayer.dispose();
-                }
-                this.mediaPlayer = mediaPlayer;
-            });
-
-            final boolean isSongPersonal = song.getUploader().getId() == user.getId();
-
-            final CardItem cardItem = SongUtils.songToCardItem(song);
-
-            Platform.runLater(() -> {
-                this.deleteSongButton.setVisible(isSongPersonal);
-                this.deleteSongButton.setManaged(isSongPersonal);
-
-                if (this.songService.isSongLiked(song)) {
-                    this.likeSongButton.getStyleClass().addAll(Styles.DANGER);
-                    this.likeSongButton.setGraphic(FAVORITE_FONT_ICON);
-                } else {
-                    this.likeSongButton.getStyleClass().removeAll(Styles.DANGER);
-                    this.likeSongButton.setGraphic(NOT_FAVORITE_FONT_ICON);
-                }
-
-                this.songCard.setCardItem(cardItem);
-
-                this.slider.setMax(song.getLengthMillis() == 0 ? 1 : song.getLengthMillis());
-
-                getChildren().setAll(this.gridPane);
-                updateMediaPlayerStatus();
-            });
-        }
-    }
-
-    private void updateSongCurrentTime() {
-        final Duration newTime = getSkinnable().currentSongTimeProperty().get();
-        if (this.mediaPlayer != null) {
-            final Duration currentTime = this.mediaPlayer.getCurrentTime();
-            final double diff = Math.abs(newTime.toMillis() - currentTime.toMillis());
-            if (diff > MIN_DIFF_TO_SEEK.toMillis()) {
-                this.mediaPlayer.seek(newTime);
-            }
-        }
-        this.slider.setValue(newTime.toMillis());
+        registerChangeListener(getSkinnable().isSongPersonalProperty(), observable -> updateIsSongPersonal());
+        updateIsSongPersonal();
     }
 
     private void updateMediaPlayerStatus() {
@@ -170,8 +100,72 @@ public class SongViewSkin extends SkinBase<SongView> {
         }
     }
 
+    private void updateSongCurrentTime() {
+        final Duration newTime = getSkinnable().currentSongTimeProperty().get();
+
+        if (this.mediaPlayer != null) {
+            final Duration currentTime = this.mediaPlayer.getCurrentTime();
+            final double diff = Math.abs(newTime.toMillis() - currentTime.toMillis());
+            if (diff > MIN_DIFF_TO_SEEK.toMillis()) {
+                this.mediaPlayer.seek(newTime);
+            }
+        }
+        this.slider.setValue(newTime.toMillis());
+    }
+
+    private void updateCurrentSong() {
+        final Song song = getSkinnable().currentSongProperty().get();
+
+        if (song == null) {
+            Platform.runLater(() -> {
+                getChildren().setAll(this.loader);
+            });
+        } else {
+            createMediaPlayer(song).thenAccept(mediaPlayer -> {
+                if (this.mediaPlayer != null) {
+                    this.mediaPlayer.dispose();
+                }
+                this.mediaPlayer = mediaPlayer;
+            });
+
+            final CardItem cardItem = SongUtils.songToCardItem(song);
+
+            Platform.runLater(() -> {
+                this.songCard.setCardItem(cardItem);
+
+                this.slider.setMax(song.getLengthMillis() == 0 ? 1 : song.getLengthMillis());
+
+                getChildren().setAll(this.gridPane);
+                updateMediaPlayerStatus();
+            });
+        }
+    }
+
+    private void updateIsSongLiked() {
+        final boolean isSongLiked = getSkinnable().isSongLikedProperty().get();
+
+        Platform.runLater(() -> {
+            if (isSongLiked) {
+                this.likeSongButton.getStyleClass().addAll(Styles.DANGER);
+                this.likeSongButton.setGraphic(FAVORITE_FONT_ICON);
+            } else {
+                this.likeSongButton.getStyleClass().removeAll(Styles.DANGER);
+                this.likeSongButton.setGraphic(NOT_FAVORITE_FONT_ICON);
+            }
+        });
+    }
+
+    private void updateIsSongPersonal() {
+        final boolean isSongPersonal = getSkinnable().isSongPersonalProperty().get();
+
+        Platform.runLater(() -> {
+            this.deleteSongButton.setVisible(isSongPersonal);
+            this.deleteSongButton.setManaged(isSongPersonal);
+        });
+    }
+
     private CompletableFuture<MediaPlayer> createMediaPlayer(Song song) {
-        return this.songService.fetchSongAudioStreamUrl(song).thenApply(audioStreamUrl -> {
+        return getSkinnable().fetchSongAudioStreamUrl(song).thenApply(audioStreamUrl -> {
             final Media media = new Media(audioStreamUrl);
 
             final MediaPlayer mediaPlayer = new MediaPlayer(media);
@@ -189,89 +183,24 @@ public class SongViewSkin extends SkinBase<SongView> {
         });
     }
 
-    private Optional<Playlist> openAddToPlaylistDialog() {
-        final Dialog<Playlist> dialog = new Dialog<>();
-        dialog.setTitle("Add to playlist");
-
-        final HBox hBox = new HBox(Config.GAP_LARGE);
-        hBox.setPadding(new Insets(Config.GAP_LARGE));
-        hBox.setAlignment(Pos.CENTER);
-
-        final Label playlistLabel = new Label("Choose playlist:");
-
-        final ComboBox<Playlist> playlistComboBox = createPlaylistComboBox();
-
-        hBox.getChildren().addAll(playlistLabel, playlistComboBox);
-
-        dialog.getDialogPane().setContent(hBox);
-        dialog.getDialogPane().getButtonTypes().addAll(
-            new ButtonType("Add", ButtonBar.ButtonData.OK_DONE),
-            new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE));
-
-        dialog.setResultConverter(button -> {
-            if (button.getButtonData() == ButtonBar.ButtonData.OK_DONE) {
-                return playlistComboBox.getValue();
-            }
-            return null;
-        });
-
-        return dialog.showAndWait();
-    }
-
-    private ComboBox<Playlist> createPlaylistComboBox() {
-        final ObservableList<Playlist> playlists = this.playlistService.playlistsProperty();
-        final User user = this.authService.getLoggedUser();
-        final FilteredList<Playlist> personal = playlists.filtered(
-            p -> p.getOwner().getId() == user.getId());
-
-        final ComboBox<Playlist> playlistComboBox = new ComboBox<>(personal);
-        playlistComboBox.setPlaceholder(new Label("Loading..."));
-        playlistComboBox.setConverter(new StringConverter<>() {
-            @Override
-            public String toString(Playlist object) {
-                return object == null ? "Choose playlist..." : object.getName();
-            }
-
-            @Override
-            public Playlist fromString(String string) {
-                return playlistComboBox.getItems().stream().filter(p ->
-                    Objects.equals(p.getName(), string)).findAny().orElse(null);
-            }
-        });
-
-        return playlistComboBox;
-    }
-
     private Button createLikeSongButton() {
-        final Button button = new Button();
-
+        final Button button = new Button(null, FAVORITE_FONT_ICON);
         button.getStyleClass().addAll(Styles.BUTTON_ICON, Styles.ROUNDED);
-
-        button.setOnAction(event -> {
-            final Song song = this.songService.getCurrentSong();
-
-            this.songService.toggleLikeSong(song);
-        });
+        button.setOnAction(event -> getSkinnable().onLikeSongButtonClick());
         return button;
     }
 
     private Button createAddToPlaylistButton() {
         final Button button = new Button("Add to Playlist", new FontIcon(Material2AL.FEATURED_PLAY_LIST));
         button.getStyleClass().add(Styles.ACCENT);
-        button.setOnAction(event -> {
-            final Optional<Playlist> result = openAddToPlaylistDialog();
-
-            result.ifPresent(playlist -> getSkinnable().addCurrentSongToPlaylist(playlist));
-        });
+        button.setOnAction(event -> getSkinnable().onAddToPlaylistButtonClick());
         return button;
     }
 
     private Button createDeleteSongButton() {
         final Button button = new Button("Delete Song", DELETE_FONT_ICON);
         button.getStyleClass().add(Styles.DANGER);
-        button.setOnAction(event -> {
-            getSkinnable().deleteCurrentSong();
-        });
+        button.setOnAction(event -> getSkinnable().onDeleteSongButtonClick());
         return button;
     }
 
